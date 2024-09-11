65d5c90e9bb9bd372d63de1ad5641e76909bfbd3
Srejon Joy <89932669+SrejonJoy@users.noreply.github.com>
2024-09-11 22:16:03 +0600

    css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 // pages/_app.tsx
 import { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
